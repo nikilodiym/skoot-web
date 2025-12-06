0fcec2d20a9f25a1c76c6ef2a78c04eb9e241fe6
Нікіта Креньов <krenov@ITStep.local>
2025-12-06 16:57:16 +0200

    commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,21 +6,24 @@ export default function Header() {
 
     return (
         <header className="header">
-            <h1 className="header-logo"><a href="/">scoot</a></h1>
-
-            <nav className={`header-nav ${open ? "open" : ""}`}>
-                <a href="/about">About</a>
-                <a href="/location">Location</a>
-                <a href="/careers">Careers</a>
-            </nav>
-
-            <button className="header-btn">Get Scootin</button>
 
             <div className="burger" onClick={() => setOpen(!open)}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
+            <h1 className="header-logo"><a href="/">scoot</a></h1>
+
+            <nav className={`header-nav ${open ? "open" : ""}`}>
+                <a href="/about">About</a>
+                <a href="/location">Location</a>
+                <a href="/careers">Careers</a>
+                <a href="" className="added-in-mobil-arrientation-block">Get Scootin</a>
+            </nav>
+
+            <button className="header-btn">Get Scootin</button>
+
+
         </header>
     );
 }
